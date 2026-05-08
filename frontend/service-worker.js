e1e36dc18d56365a1e7ce742714a9515a588d633
Yani-cnvs <yani.contreras.v@gmail.com>
2026-05-08 00:07:31 -0400

    Mejorando service worker

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,6 +1,53 @@
-self.addEventListener('install', e => {
-    console.log('Service Worker instalado!');
+// Nombre de caché de la aplicación
+const nombreCache= 'localsmart-v1';
+
+// Archivos que se guardarán para uso offline
+const archivosCache= [
+    '/',
+    '/index.html',
+    '/dashboard.html',
+    '/productos.html',
+    '/tareas.html',
+    '/usuarios.html',
+    '/incentivos.html',
+    '/reportes.html',
+    '/style.css',
+    '/app.js',
+    '/login.js',
+    '/productos.js',
+    '/tareas.js',
+    '/usuarios.js',
+    '/reportes.js',
+    '/manifest.json'
+];
+
+// Se ejecuta cuando el Service Worker se instala
+self.addEventListener('install', evento =>{
+    console.log ('Service Worker instalado!');
+
+    evento.waitUntil (
+        caches.open (nombreCache)
+        .then(cache=> {
+            console.log('Archivos guardados en caché');
+            
+            return cache.addAll(archivosCache);
+        })
+    );
+
 });
-self.addEventListener('fetch', e=> {
-    e.respondWith(fetch(e.request));
+
+// Se ejecuta cada vez que la aplicación hace una petición
+self.addEventListener('fetch', evento => {
+
+    evento.respondWith(
+
+        caches.match(evento.request)
+        .then(respuestaCache => {
+
+        if(respuestaCache) {
+            return respuestaCache;
+            }
+            return fetch(evento.request);
+        })
+    );
 });
