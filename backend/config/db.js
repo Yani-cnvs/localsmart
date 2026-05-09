624b9735c1de35a23e1ed4733f9e9ecb13a9b819
Yani-cnvs <yani.contreras.v@gmail.com>
2026-05-09 08:54:46 -0400

    Limpiando  estructura, eliminando duplicados y node_modules de raiz

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,21 @@
 const mysql = require('mysql2/promise');
 
-const connection = mysql.createPool ({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_NAME:', process.env.DB_NAME);
+
+const connection = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+      ssl: {
+    rejectUnauthorized: false
+  }
 });
 
+console.log('Conectado correctamente a la base de datos!');
 
 module.exports = connection;
